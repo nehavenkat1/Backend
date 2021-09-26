ca1f0012a2c4da04887b3464e88cfe47f73d3c5b
nehavenkat1 <nehavenky@yahoo.com>
2021-09-26 08:12:40 +0530

    added new functionality

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -19,13 +19,22 @@ const DUMMY_PLACES = [
 router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid
     const place = DUMMY_PLACES.find(place => place.id === placeId)
+    if(!place) {
+        const error = new Error('Could not find a place.')
+        error.code = 404
+        throw error
+    }
     res.json({place})
 })
 
 router.get('/user/:uid', (req, res, next) => {
     const userId = req.params.uid
-    console.log("user id", userId)
     const place = DUMMY_PLACES.find(place => place.creator === userId)
+    if(!place) {
+        const error = new Error('Could not find a place for given user id.')
+        error.code = 404
+        return next(error)
+    }
     res.json({place})
 })
 module.exports = router
