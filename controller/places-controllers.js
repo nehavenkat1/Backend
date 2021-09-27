600ea408c503da31992dff57fc0a641627116227
nehavenkat1 <nehavenky@yahoo.com>
2021-09-27 08:21:05 +0530

    added new files

--- controller/places-controllers.js
+++ controller/places-controllers.js
@@ -1,3 +1,5 @@
+const uuid = require('uuid')
+
 const HttpError = require('../models/http-error')
 
 const DUMMY_PLACES = [
@@ -32,5 +34,42 @@ const getPlaceByUserId = (req, res, next) => {
     res.json({place})
 }
 
+const createPlace = (req, res, next) => {
+    const { title, description, coordinates, address, creator } = req.body
+
+    const createdPlace = {
+        id: uuid.v4(),
+        title,
+        description,
+        location: coordinates,
+        address,
+        creator
+    }
+
+    DUMMY_PLACES.push(createdPlace)
+    res.status(201).json({place: createdPlace})
+}
+
+const updatePlaceById = (req, res, next) => {
+    const { title, description } = req.body
+    const placeId = req.params.pid
+
+    const updatedPlace = {...DUMMY_PLACES.find(p => p.id === placeId)}
+    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId)
+    updatedPlace.title = title
+    updatedPlace.description = description
+
+    DUMMY_PLACES[placeIndex] = updatedPlace
+
+    res.status(200).json({place: updatedPlace})
+}
+
+const deletePlace = (req, res, next) => {
+
+}
+
 exports.getPlaceById = getPlaceById
 exports.getPlaceByUserId = getPlaceByUserId
+exports.createPlace = createPlace
+exports.updatePlaceById = updatePlaceById
+exports.deletePlace = deletePlace
